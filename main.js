231fccce42363d04b878970df38cd796ab2e3e95
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-10-31 23:37:05 +0000

    completed rock paper scissors programme

--- main.js
+++ main.js
@@ -1,62 +1,76 @@
-// Need user input
+// User input function
 
-var input = prompt("Enter 'ROCK', 'PAPER' or 'SCISSORS'");
-
-// Need random computer input
-var comp = Math.random() * 3;
-if (comp <= 1) {
-  output = "ROCK";
-}
-else if (comp <= 2) {
-  output = "PAPER";
-}
-else {
-  output = "SCISSORS";
+function inputFunction() {
+  var input = prompt("Enter 'ROCK', 'PAPER' or 'SCISSORS'");
+  return input;
 }
 
-console.log(output);
-// Need test for who wins
+// Computer random generator
 
-function test(){
+function computerChoice(){
+  var output = 0;
+  var comp = Math.random() * 3;
+  if (comp <= 1) {
+    output = "ROCK";
+  }
+  else if (comp <= 2) {
+    output = "PAPER";
+  }
+  else {
+    output = "SCISSORS";
+  }
+  return output;
+}
+
+// Decide winner function
+
+function winner() {
+
+  var user = inputFunction();
+  var computer = computerChoice();
   var message = 0;
+
+  console.log(user);
+  if (user == computer) {
+    return 0;
+  } else if(user === "ROCK" && computer === "SCISSORS") {
+    return 1;
+  } else if (user == "ROCK" && computer == "PAPER") {
+    return 2;
+  } else if (user == "SCISSORS" && computer == "ROCK") {
+    return 2;
+  } else if (user == "SCISSORS" && computer == "PAPER") {
+    return 1;
+  } else if (user == "PAPER" && computer == "ROCK") {
+    return 1;
+  } else if (user == "PAPER" && computer == "SCISSORS") {
+    return 2;
+  }
+}
+
+// main code
+
+var check = 0;
+while (check == 0){
   var playerScore = 0;
   var computerScore = 0;
-  for (var i = 1; i < 2; i++) {
-    while (playerScore < 3 && computerScore < 3) {
-      if (input === "ROCK" && output === "SCISSORS") {
-        playerScore ++;
-        message = "YOU WIN THIS ROUND!"
-      } else if (input === "ROCK" && output === "PAPER") {
-        computerScore ++;
-        message = "YOU LOSE THIS ROUND!";
-      } else if (input === "SCISSORS" && output === "ROCK") {
-        computerScore ++;
-        message = "YOU LOSE THIS ROUND!";
-      } else if (input === "SCISSORS" && output === "PAPER") {
-        playerScore ++;
-        message = "YOU WIN THIS ROUND!"
-      } else if (input === "PAPER" && output === "ROCK") {
-        playerScore ++;
-        message = "YOU WIN THIS ROUND!"
-      } else if (input === "PAPER" && output === "SCISSORS") {
-        computerScore ++;
-        message = "YOU LOSE THIS ROUND!";
-      } else if (input === output) {
-        message = "IT IS A DRAW.";
-      } else {
-        alert("Error");
-      }
-    }
-    if (playerScore === 3) {
-      alert("You win!");
-    } else if (computerScore) {
-      alert("You lose!");
+  while (playerScore <= 2 && computerScore <= 2) {
+    var whoWon = winner();
+    if (whoWon === 0){
+      alert("It's a DRAW!" + "\n\nSCORE: " + playerScore + " - " + computerScore);
+    } else if (whoWon == 1){
+      playerScore ++;
+      alert("You WIN this round!" + "\n\nSCORE: " + playerScore + " - " + computerScore);
+    } else if (whoWon == 2){
+      computerScore ++;
+      alert("You LOSE this round!" + "\n\nSCORE: " + playerScore + " - " + computerScore);
     }
   }
-  console.log(input);
+  if (playerScore === 3){
+    alert("Congratulation you've WON!!!")
+  }
+  else if (computerScore === 3){
+    alert("Unfortunately you LOST this time...")
+  }
+  check = 1;
 }
-
-
-
-test();
-// Need counter for wins and losses

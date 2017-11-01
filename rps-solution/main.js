ce78a4ebe05b0ba94b265dd1b7b64f02a3c9affc
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-01 13:36:50 +0000

    completed homework solution lab

--- rps-solution/main.js
+++ rps-solution/main.js
@@ -3,11 +3,6 @@ function getUserInput() {
   return prompt("Please choose either 'rock', 'paper' or 'scissors'.");
 }
 
-// Get the player move
-function getPlayerMove() {
-  return getUserInput();
-}
-
 // Random player
 function randomPlay() {
   var randomNumber = Math.floor(Math.random() * 3);
@@ -20,14 +15,58 @@ function randomPlay() {
   }
 }
 
+// Get the player move
+function getPlayerMove(playerMove) {
+  return playerMove || getUserInput();
+}
 
 // get the computer move
+function getComputerMove(computerMove) {
+  return computerMove || randomPlay();
+}
 
-var test = randomPlay();
-console.log(test);
+// var test = getComputerMove();
+// console.log("This is the computer move:", test);
 
 // will need to play to five
+function startGame() {
+  alert("Let's play rock paper scissors")
+  // score variables
+  var playerScore = 0;
+  var computerScore = 0;
+
+  while (playerScore < 5 && computerScore < 5) {
+    var playerMove = getPlayerMove();
+    var computerMove = getComputerMove();
+    var winner = checkForWinner(playerMove, computerMove);
+
+    if (winner === "player"){
+      playerScore ++;
+      alert("The player won that round with - " + playerMove + "against - " + computerMove + "-------" + "The scores are --- Player " + playerScore + "Computer score --- " + computerScore);
+    } else if (winner === "computer"){
+      computerScore ++;
+      alert("The computer won that round with - " + computerMove + "against - " + playerMove + "-------" + "The scores are --- Player " + playerScore + "Computer score --- " + computerScore);
+    } else {
+      alert("That was a tie with - " + playerMove + computerMove)
+    }
+  }
+}
 
 // check for winner
+function checkForWinner(playerMove, computerMove) {
+  var winner;
 
-//
+  if (playerMove === computerMove) {
+    winner = "tie";
+    winner;
+  } else if ((playerMove === "rock" && computerMove === "scissors") || (playerMove === "paper" && computerMove === "rock") || (playerMove === "scissors" && computerMove === "paper")) {
+    winner = "player";
+  } else {
+    winner = "computer";
+  }
+  return winner;
+}
+
+
+
+startGame();

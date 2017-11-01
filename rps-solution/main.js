02709c26e7b8eaab3362307e6152a67faeea38ef
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-01 11:20:56 +0000

    added random computer input generator

--- rps-solution/main.js
+++ rps-solution/main.js
@@ -1,30 +1,30 @@
 // Get input
-
 function getUserInput() {
   return prompt("Please choose either 'rock', 'paper' or 'scissors'.");
 }
 
-var test = getUserInput();
-console.log(test);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Get the player move
+function getPlayerMove() {
+  return getUserInput();
+}
 
 // Random player
+function randomPlay() {
+  var randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return "rock";
+  } else if (randomNumber === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
 
 // get the computer move
+
+var test = randomPlay();
+console.log(test);
 
 // will need to play to five
 
